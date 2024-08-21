fd27f27482e1c0c619aafbb272900f2da5ca6c35
Paulo <pmuraroferreira@gmail.com>
2024-08-20 21:15:33 -0300

    update pyproject with the packages.find

--- src/workflow.py
+++ src/workflow.py
@@ -1,15 +1,18 @@
 from src.agent import AgentState, StateGraph, Agent
 from langchain.globals import set_debug
 from src.logger_setup import logger
+from langgraph.checkpoint.memory import MemorySaver
 
 class WorkFlow:
 
     def __init__(self, prompt, plotting_graph_structure=False, debug=False) -> None:
         
-        set_debug(debug)
+        set_debug(debug)    
         self.plotting_graph_structure = plotting_graph_structure
         self.prompt = prompt
         self.agent = Agent()
+        self.memory = MemorySaver()
+        self.thread = {"configurable": {"thread_id": "2"}}
 
     def setting_workflow(self):
 
@@ -24,7 +27,7 @@ class WorkFlow:
         self.workflow.add_edge("execute_python", "call_model")
         self.workflow.add_conditional_edges("call_model", self.agent.should_continue)
 
-        self.app = self.workflow.compile()
+        self.app = self.workflow.compile(checkpointer=self.memory, interrupt_before=["execute_sql_query"])
 
     def running_agent(self):
 
@@ -32,7 +35,11 @@ class WorkFlow:
         if self.plotting_graph_structure:
             self.app.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
-        output = self.app.invoke({"messages": [("human", self.prompt)]})
+        #output = self.app.invoke({"messages": [("human", self.prompt)]})
+        for event in self.app.stream({"messages": [("human", self.prompt)]}, self.thread, stream_mode="values"):
+            logger.info(event["messages"])
+
+        logger.error(event[-1])
         
 
 if __name__ == "__main__":
